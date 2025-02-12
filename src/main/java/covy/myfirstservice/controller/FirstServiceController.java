c4a0dd9af1c9e9ab6543a3f2807c2e8224e6ea38
박준혁/PD프로젝트 <junni802@gsretail.com>
2025-02-12 17:22:36 +0900

    feat: fix

---
+++ src/main/java/covy/myfirstservice/controller/FirstServiceController.java
@@ -0,0 +1,23 @@
+package covy.myfirstservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <클래스 설명>
+ *
+ * @author : junni802
+ * @date : 2025-02-12
+ */
+
+@RestController
+@RequestMapping("/")
+public class FirstServiceController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the First service";
+    }
+
+}
