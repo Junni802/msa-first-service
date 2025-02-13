54fe5b7191e64a0e22f260ef672687200d7e4e50
박준혁/PD프로젝트 <junni802@gsretail.com>
2025-02-13 15:30:58 +0900

    feat: fix

--- src/main/java/covy/myfirstservice/controller/FirstServiceController.java
+++ src/main/java/covy/myfirstservice/controller/FirstServiceController.java
@@ -1,6 +1,7 @@
 package covy.myfirstservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/first-service")
 public class FirstServiceController {
 
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the First service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        System.out.println(header);
+        return "Hello World in First Service.";
     }
 
 }
